db57436fd351ccb8f5bcb4978c1352d6cda71b73
Rene van der Ark <r.van.der.ark@gmail.com>
2017-10-23 10:09:29 +0200

    more scaling support, output 100% quality.

--- src/main/java/nl/kb/iiif/api/ScaleDims.java
+++ src/main/java/nl/kb/iiif/api/ScaleDims.java
@@ -1,5 +1,7 @@
 package nl.kb.iiif.api;
 
+import com.google.common.collect.Lists;
+
 public class ScaleDims {
     @Override
     public String toString() {
@@ -17,11 +19,23 @@ public class ScaleDims {
         this.h = h;
     }
 
+    private ScaleDims(int... dims) {
+        this.w = dims[0];
+        this.h = dims[1];
+    }
+
     public static ScaleDims parseAndDetermine(String sizeParam, Region region) {
         if (sizeParam.matches("^pct:[0-9.]+$")) {
             final double s = Double.parseDouble(sizeParam.replaceAll("^pct:", "")) / 100.0;
-            return new ScaleDims((int)Math.round(s * region.getW()), (int) Math.round(s * region.getH()));
-
+            return new ScaleDims((int) Math.round(s * region.getW()), (int) Math.round(s * region.getH()));
+        } else if (sizeParam.matches("^[0-9]+,[0-9]+$")) {
+            final int[] dims = Lists.newArrayList(sizeParam.split(","))
+                    .stream().mapToInt(Integer::parseInt).toArray();
+            return new ScaleDims(dims);
+        } else if (sizeParam.matches("^[0-9]+,$")) {
+            final int w = Integer.parseInt(sizeParam.replaceAll(",", ""));
+            final int h = (int) Math.ceil(((double) w / (double) region.getW()) * region.getH());
+            return new ScaleDims(w, h);
         } else /* sizeParam = full or max*/ {
           return new ScaleDims(region.getW(), region.getH());
         }
--- src/main/java/nl/kb/iiif/resources/ImageResource.java
+++ src/main/java/nl/kb/iiif/resources/ImageResource.java
@@ -5,11 +5,16 @@ import nl.kb.iiif.api.Region;
 import nl.kb.iiif.api.ScaleDims;
 import nl.kb.iiif.core.ImageFetcher;
 import nl.kb.image.BufferedImageWriter;
+import nl.kb.jp2.DimReducer;
 import nl.kb.jp2.Jp2Decode;
 import nl.kb.jp2.Jp2Header;
-import nl.kb.jp2.DimReducer;
 
+import javax.imageio.IIOImage;
 import javax.imageio.ImageIO;
+import javax.imageio.ImageWriteParam;
+import javax.imageio.ImageWriter;
+import javax.imageio.plugins.jpeg.JPEGImageWriteParam;
+import javax.imageio.stream.MemoryCacheImageOutputStream;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
@@ -85,9 +90,15 @@ public class ImageResource {
                     scaleDims.getW(), scaleDims.getH()
             );
 
-
+            final JPEGImageWriteParam jpegParams = new JPEGImageWriteParam(null);
+            final ImageWriter writer = ImageIO.getImageWritersByFormatName("jpg").next();
             final ByteArrayOutputStream baos = new ByteArrayOutputStream();
-            ImageIO.write(image, "jpg", baos);
+
+            jpegParams.setCompressionMode(ImageWriteParam.MODE_EXPLICIT);
+            jpegParams.setCompressionQuality(1f);
+
+            writer.setOutput(new MemoryCacheImageOutputStream(baos));
+            writer.write(null, new IIOImage(image, null, null), jpegParams);
 
             return Response.ok(baos.toByteArray()).build();
         } catch (IOException e) {
