3980e594d8c4bc4e7b6e5e8435e2601d953fc3b5
Rene van der Ark <r.van.der.ark@gmail.com>
2017-11-02 10:03:38 +0100

    backward build.

--- src/main/native/compile.sh
+++ src/main/native/compile.sh
@@ -1,8 +1,17 @@
 #!/usr/bin/env bash
 
 cd src/main/native
-gcc -I /usr/local/include/openjpeg-2.3 \
-    -I $JAVA_HOME/include -I $JAVA_HOME/include/linux -fpic \
-    -shared -o ../resources/native/libjp2j.so log.c opj_res.c nl_kb_jp2_Jp2Header.c nl_kb_jp2_Jp2Decode.c \
-    -lopenjp2
+if [ "$1" = "2.0" ]; then
+    echo "compiling version 2.0"
+    gcc -I /usr/local/include/openjpeg-2.0 \
+        -I $JAVA_HOME/include -I $JAVA_HOME/include/linux -fPIC \
+        -shared -o ../resources/native/libjp2j.so log.c opj_res_2.0.c nl_kb_jp2_Jp2Header.c nl_kb_jp2_Jp2Decode.c \
+        -lopenjp2
+else
+    echo "compiling version 2.3"
+    gcc -I /usr/local/include/openjpeg-2.3 \
+        -I $JAVA_HOME/include -I $JAVA_HOME/include/linux -fPIC \
+        -shared -o ../resources/native/libjp2j.so log.c opj_res.c nl_kb_jp2_Jp2Header.c nl_kb_jp2_Jp2Decode.c \
+        -lopenjp2
+fi
 cd ../../../
