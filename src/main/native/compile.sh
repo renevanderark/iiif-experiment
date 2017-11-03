6d3c2aba50e0236796f6d55597849a3312416f6a
Rene <r.van.der.ark@gmail.com>
2017-11-03 09:10:21 +0100

    mrg.

--- src/main/native/compile.sh
+++ src/main/native/compile.sh
@@ -4,13 +4,13 @@ cd src/main/native
 if [ "$1" = "2.0" ]; then
     echo "compiling version 2.0"
     gcc -I /usr/local/include/openjpeg-2.0 \
-        -I $JAVA_HOME/include -I $JAVA_HOME/include/linux -fPIC \
+        -I $JAVA_HOME/include -I $JAVA_HOME/include/linux -fPIC -std=c99 \
         -shared -o ../resources/native/libjp2j.so log.c opj_res_2.0.c nl_kb_jp2_Jp2Header.c nl_kb_jp2_Jp2Decode.c \
         -lopenjp2
 else
     echo "compiling version 2.3"
     gcc -I /usr/local/include/openjpeg-2.3 \
-        -I $JAVA_HOME/include -I $JAVA_HOME/include/linux -fPIC \
+        -I $JAVA_HOME/include -I $JAVA_HOME/include/linux -fPIC -std=c99 \
         -shared -o ../resources/native/libjp2j.so log.c opj_res.c nl_kb_jp2_Jp2Header.c nl_kb_jp2_Jp2Decode.c \
         -lopenjp2
 fi
